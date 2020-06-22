b52a027ce18428a6a052aaa9a355d596d4399e9b
João Pedro Benedetti Misturini <joao.benedetti.misturini@gmail.com>
2020-06-22 01:36:51 +0000

    adiciona abertura de OS

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,33 +1,51 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Navbar = props => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link to="/" className="navbar-brand">Ponto de venda</Link>
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props)
 
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
-            <span className="navbar-toggler-icon"></span>
-        </button>
+        this.state = { menuOpen: false }
+    }
 
-        <div className="collapse navbar-collapse" id="navbar">
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                    <Link to="/fornecedores" className="nav-link">Fornecedores</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/compras" className="nav-link">Compras</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/abrir-caixa" className="nav-link">Abrir caixa</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/consultar-saldo" className="nav-link">Consultar saldo</Link>
-                </li>
-            </ul>
+    handleClick = () => {
+        this.setState(({ menuOpen }) => ({
+            menuOpen: ! menuOpen,
+        }))
+    }
 
-            <button type="button" onClick={ props.onLogout } className="btn btn-outline-danger">Sair</button>
-        </div>
-    </nav>
-)
+    render = () => (
+        <nav className="navbar navbar-expand-md navbar-light bg-light">
+            <Link to="/" className="navbar-brand">Ponto de venda</Link>
+
+            <button type="button" className="navbar-toggler" onClick={ this.handleClick }>
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className={ `collapse navbar-collapse ${ this.state.menuOpen ? 'show' : '' }` }>
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link to="/fornecedores" className="nav-link">Fornecedores</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/compras" className="nav-link">Compras</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/abrir-caixa" className="nav-link">Abrir caixa</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/consultar-saldo" className="nav-link">Consultar saldo</Link>
+                    </li>
+                </ul>
+
+                <button type="button" onClick={ this.props.onLogout }
+                    className="btn btn-outline-danger my-2 my-md-0"
+                >
+                    Sair
+                </button>
+            </div>
+        </nav>
+    )
+}
 
 export default Navbar
