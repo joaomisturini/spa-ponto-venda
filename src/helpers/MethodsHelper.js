6b77eedcaf868eee32f8fe0ff2f684532dc3631a
João Pedro Benedetti Misturini <joao.benedetti.misturini@gmail.com>
2020-06-25 23:59:09 +0000

    adiciona métodos de helper

--- src/helpers/MethodsHelper.js
+++ src/helpers/MethodsHelper.js
@@ -1,4 +1,19 @@
 import Bus from './BusHelper'
+import numeral from 'numeral'
+
+numeral.register('locale', 'pt-BR', { delimiters: {
+    thousands: '.', decimal: ',',
+} })
+
+numeral.locale('pt-BR')
+
+export const cnpj = string => string.replace(
+    /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
+    '$1.$2.$3/$4-$5'
+)
+
+export const float = string => string.toString()
+    .replace(/[,]/g, '.').replace(/[.](?=.*[.])/g, '')
 
 export const handleError = async (callback, errorData) => {
     try {
@@ -13,3 +28,10 @@ export const handleError = async (callback, errorData) => {
         return errorData
     }
 }
+
+export const money = number => numeral(number).format('0,0.00')
+
+export const phone = string => string.replace(
+    /^(\d{2})(\d{4})(\d{4})/,
+    '($1) $2-$3'
+)
