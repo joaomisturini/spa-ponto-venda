3181628ba436ab60dde4d3c0ad00904d88e36dc6
João Pedro Benedetti Misturini <joao.benedetti.misturini@gmail.com>
2020-06-26 23:54:35 +0000

    finaliza dashboard

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import UserService from '../../services/UserService'
 
-const ScreensDashboard = () => (
-    <div className="row">
-        <div className="col px-5 py-4">
-            <h1 className="mb-4">Seja bem-vindo!</h1>
-            <p>Selecione uma das opções do menu para iniciar.</p>
+class ScreensDashboard extends React.Component {
+    state = { name: ' '}
+
+    componentDidMount = async () => {
+        const user = await UserService.show()
+
+        this.setState({ name: user.name || '' })
+    }
+
+    render = () => (
+        <div className="row">
+            <div className="col px-5 py-4">
+                <h1 className="mb-4">Seja bem-vindo, { this.state.name }!</h1>
+                <p>Selecione uma das opções do menu para iniciar.</p>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default ScreensDashboard
