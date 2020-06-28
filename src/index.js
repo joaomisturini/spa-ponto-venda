646ae44cf5642730186825f905b1ae24f7dac4b2
João Pedro Benedetti Misturini <joao.benedetti.misturini@gmail.com>
2020-06-28 14:52:04 +0000

    remove ponto-vírgula

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
     <React.StrictMode><App /></React.StrictMode>,
     document.getElementById('root')
-);
+)
