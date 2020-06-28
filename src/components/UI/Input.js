0b9d38a08442427ad6a98c99fc942158967b6770
João Pedro Benedetti Misturini <joao.benedetti.misturini@gmail.com>
2020-06-28 22:54:09 +0000

    ajusta inputs

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const Input = ({ label, ...props }) => {
+const Input = ({ label, noGroup, ...props }) => {
     const htmlLabel = label ? <label>{ label }</label> : null
+
+    if (noGroup) {
+        return <input className="form-control" { ...props } />
+    }
 
     return (
         <div className="form-group">{ htmlLabel }
--- src/components/UI/Select.js
+++ src/components/UI/Select.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Select = ({ options, label, ...props }) => {
+const Select = ({ options, label, noGroup, ...props }) => {
     const htmlLabel = label ? <label>{ label }</label> : null
 
     const htmlOptions = options.map(option => (
         <option key={ option.value } value={ option.value }>{ option.text }</option>
     ))
+
+    if (noGroup) {
+        return <select className="form-control" { ...props }>{ htmlOptions }</select>
+    }
 
     return (
         <div className="form-group">{ htmlLabel }
